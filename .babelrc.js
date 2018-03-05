3a3f1df28a7d668274c59fdae66a4e46426020b1
artalar <artalar@ya.ru>
2018-03-05 06:58:46 +0300

    fix build

--- .babelrc.js
+++ .babelrc.js
@@ -11,6 +11,10 @@ module.exports = {
   ],
   plugins: [
     "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-object-rest-spread"
+    "@babel/plugin-proposal-object-rest-spread",
+    ["@babel/plugin-transform-runtime", {
+      "polyfill": false,
+      "regenerator": true
+    }]
   ]
 };
