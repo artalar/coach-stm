a417885b9acdacbdb62295142cc0a11bdcb85cec
artalar <artalar@ya.ru>
2018-03-19 02:12:06 +0300

    another rethink

--- src/index.js
+++ src/index.js
@@ -1,70 +1,13 @@
-/******************
- *** MIDDLEWARE ***
- ******************/
-
-const getDate = () => {
-  const date = new Date();
-  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}:${date.getMilliseconds()}`;
-};
-
-export const logger = async (payload, meta, task) => {
-  const { description, taskIndex, tasksCount } = meta;
-  if (taskIndex === 0) {
-    console.groupCollapsed(`Goal ${description ? `"${description}"` : ""}`);
-  }
-  console.log(`Task #${taskIndex} start at ${getDate()}`, { payload, meta });
-  try {
-    payload = await task(payload, meta);
-  } catch (e) {
-    console.error(`Task #${taskIndex} failed at ${getDate()}`, {
-      payload,
-      meta
-    });
-    console.groupEnd();
-    throw e;
-  }
-  console.log(`Task #${taskIndex} end at ${getDate()}`, { payload, meta });
-  if (taskIndex === tasksCount - 1) console.groupEnd();
-  return payload;
-};
-
-const workingTasks = new WeakMap();
-
-export const rejectedByRaceCondition = new Error("rejected by race condition");
-
-export const preventRaceCondition = async (payload, meta, task) => {
-  const { tasks, taskIndex, processId } = meta;
-  if (taskIndex === 0) {
-    workingTasks.set(tasks, processId);
-  }
-  if (workingTasks.get(tasks) !== processId) {
-    throw rejectedByRaceCondition;
-  }
-  return await task(payload, meta);
-};
-
-/************
- *** CORE ***
- ************/
-
-const executor = async ({
-  description,
-  tasks,
-  payload,
-  startIndex,
-  processId
-}) => {
+const executor = async ({ description, tasks, instanceId, payload, processId }) => {
   const tasksCount = tasks.length;
 
-  for (let taskIndex = startIndex; taskIndex < tasksCount; taskIndex++) {
+  for (let taskIndex = 0; taskIndex < tasksCount; taskIndex++) {
     const meta = {
       description,
-      tasks,
-      payload,
-      startIndex,
+      instanceId,
       processId,
       tasksCount,
-      taskIndex
+      taskIndex,
     };
 
     payload = await tasks[taskIndex](payload, meta);
@@ -73,38 +16,40 @@ const executor = async ({
   return payload;
 };
 
-const compose = (theMiddleware, task) => (payload, meta) =>
-  theMiddleware(payload, meta, task);
+const compose = (theMiddleware, task) => (payload, meta) => theMiddleware(payload, meta, task);
 
 export class Coach {
-  constructor({ middleware = [logger, preventRaceCondition] } = {}) {
+  constructor({ middleware = [] } = {}) {
     this.middleware = middleware;
   }
 
   withMiddleware(middleware) {
     return task =>
-      middleware.reduceRight(
-        (acc, theMiddleware) => compose(theMiddleware, acc),
-        task
-      );
+      middleware.reduceRight((acc, theMiddleware) => {
+        const f = compose(theMiddleware, acc);
+        f._cStmTaskName = task._cStmTaskName || task.name;
+        return f;
+      }, task);
   }
 
   goal(description, tasks = [payload => payload]) {
     // description not specified
     if (Array.isArray(description)) {
       tasks = description;
-      description = "";
+      description = '';
     }
+
+    const instanceId = Symbol();
 
     tasks = tasks.map(this.withMiddleware(this.middleware));
 
-    return (payload, startIndex = 0) =>
+    return payload =>
       executor({
         description,
         tasks,
+        instanceId,
         payload,
-        startIndex,
-        processId: Symbol()
+        processId: Symbol(),
       });
   }
 }
---
+++ src/middleware/kukerEmitter.js
@@ -0,0 +1,69 @@
+import { BaseEmitter } from 'kuker-emitters';
+
+const emit = BaseEmitter();
+
+export const kukerEmitter = async (payload, meta, task) => {
+  const { description, taskIndex, tasksCount } = meta;
+  let taskName = task._cStmTaskName || task.name;
+  taskName = taskName ? `"${taskName}" ` : '';
+  const goalName = description === undefined ? '' : `"${description}" `;
+  if (taskIndex === 0) {
+    emit({
+      kuker: true,
+      type: goalName,
+      origin: 'coach-stm',
+      label: `Goal ${goalName}[start]`,
+      state: { payload, meta },
+      color: '#f9dabf',
+    });
+  }
+  emit({
+    kuker: true,
+    type: goalName,
+    origin: 'coach-stm',
+    label: `Task #${taskIndex} ${taskName}[start]`,
+    state: { payload, meta },
+    color: '#f3e3d9',
+  });
+  try {
+    payload = await task(payload, meta);
+  } catch (e) {
+    emit({
+      kuker: true,
+      type: goalName,
+      origin: 'coach-stm',
+      label: `Task #${taskIndex} ${taskName}[error]`,
+      state: { payload, meta, error: e.message },
+      color: '#ff6464',
+    });
+    emit({
+      kuker: true,
+      type: goalName,
+      origin: 'coach-stm',
+      label: `Goal ${goalName}[error]`,
+      state: { payload, meta, error: e.message },
+      color: '#ff6464',
+    });
+
+    throw e;
+  }
+  emit({
+    kuker: true,
+    type: goalName,
+    origin: 'coach-stm',
+    label: `Task #${taskIndex} ${taskName}[end]`,
+    state: { payload, meta },
+    color: '#f3e3d9',
+  });
+  if (taskIndex === tasksCount - 1) {
+    emit({
+      kuker: true,
+      type: goalName,
+      origin: 'coach-stm',
+      label: `Goal ${goalName}[end]`,
+      state: { payload, meta },
+      color: '#f9dabf',
+    });
+  }
+  return payload;
+};
