c7e4e46e740b8afb316ceceb387a484d84abf512
artalar <artalar@ya.ru>
2018-03-31 23:08:17 +0300

    fix dumb fail

--- src/middleware/identity.js
+++ src/middleware/identity.js
@@ -1,4 +1,4 @@
-export const identity = callback => async (payload, meta, task) => {
+export const identity = async (payload, meta, task) => {
   const result = await task(payload, meta);
   return result === undefined ? payload : result;
 };
